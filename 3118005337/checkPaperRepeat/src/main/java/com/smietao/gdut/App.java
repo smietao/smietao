6cdf7361f52b1067cea005474858f8e07f5f78d4
smietao <709327956@qq.com>
2020-09-21 22:31:10 +0800

    feat:新建一个学号为名的文件夹，并初始化项目

---
+++ 3118005337/checkPaperRepeat/src/main/java/com/smietao/gdut/App.java
@@ -0,0 +1,13 @@
+package com.smietao.gdut;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
