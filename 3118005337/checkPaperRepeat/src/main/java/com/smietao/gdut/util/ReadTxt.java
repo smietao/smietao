5473a61b8a2c37a477b9c5945615fe5b278738fc
smietao <709327956@qq.com>
2020-09-22 23:14:39 +0800

    feat(util):添加将txt文件中的文本转换为String类型的静态方法

---
+++ 3118005337/checkPaperRepeat/src/main/java/com/smietao/gdut/util/ReadTxt.java
@@ -0,0 +1,24 @@
+package com.smietao.gdut.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class ReadTxt {
+
+    public static String txt2String(File file) {
+        StringBuilder result = new StringBuilder();
+        try {
+            //构造一个BufferedReader类来读取文件
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String s = null;
+            while ((s = br.readLine()) != null) {//使用readLine方法，一次读一行
+                result.append(System.lineSeparator()).append(s);
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+}
